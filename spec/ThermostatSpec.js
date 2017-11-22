fa1a0c89b587fe27af804fb5bbb00a4c39c3c171
antonio <antoniobelmardacosta@gmail.com>
2017-11-22 13:11:08 +0000

    Add beforeEach block that defines thermostat to ThermostatSpec

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,29 +1,34 @@
-describe("Start temperature", function() {
-  it("Should have a starting temperature of 20 degrees", function() {
-    var thermostat = new Thermostat();
-    expect(thermostat._temperature).toEqual(20)
-  });
-});
+describe("Thermostat", function() {
 
-describe("Increase temperature", function() {
-  it("Should increase the temperature by 1", function() {
-    var thermostat = new Thermostat();
-    thermostat.increaseTemperature();
-    expect(thermostat._temperature).toEqual(21)
-  });
-});
+  var thermostat
 
-describe("Decrease temperature", function() {
-  it("Should decrease the temperature by 1", function() {
-    var thermostat = new Thermostat();
-    thermostat.decreaseTemperature();
-    expect(thermostat._temperature).toEqual(19)
+  beforeEach(function() {
+    thermostat = new Thermostat();
   });
-});
 
-describe("Minimum temperature", function() {
-  it("Should have a minimum temperature set to 10 degrees", function() {
-    var thermostat = new Thermostat();
-    expect(thermostat._MINIMUM_TEMPERATURE).toEqual(10)
+  describe("Start temperature", function() {
+    it("Should have a starting temperature of 20 degrees", function() {
+      expect(thermostat._temperature).toEqual(20)
+    });
+  });
+
+  describe("Increase temperature", function() {
+    it("Should increase the temperature by 1", function() {
+      thermostat.increaseTemperature();
+      expect(thermostat._temperature).toEqual(21)
+    });
+  });
+
+  describe("Decrease temperature", function() {
+    it("Should decrease the temperature by 1", function() {
+      thermostat.decreaseTemperature();
+      expect(thermostat._temperature).toEqual(19)
+    });
+  });
+
+  describe("Minimum temperature", function() {
+    it("Should have a minimum temperature set to 10 degrees", function() {
+      expect(thermostat._MINIMUM_TEMPERATURE).toEqual(10)
+    });
   });
 });
