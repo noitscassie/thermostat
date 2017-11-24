9fc36dab2f56fff63d57edaaa12533b474159aaa
Peter Johnstone <peter.wdj@gmail.com>
2017-11-24 11:38:45 +0000

    Add API request to Open Weather Map to get temperature in London

--- jQuery.js
+++ jQuery.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
   $("#temperature").html(thermostat.getTemperature());
   $("#power-saving-status").html(thermostat.powerSavingStatus());
   $("#energy-usage").html(thermostat.currentEnergyUsage());
+  $.get("http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&APPID=24496bffc3005d5b7c520dbcc7427f2f", function(weather) {
+    $("#london-temperature").text(weather.main.temp);
+  });
 
   $("#increase-temperature").click(function() {
     thermostat.increaseTemperature();
